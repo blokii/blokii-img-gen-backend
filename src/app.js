13d6b06f4e3a107f5f490947752e44bdf792ba4f
Rachel Cheuk <hello@rachelcheuk.com>
2021-01-09 14:28:37 -0600

    updated with frontend

--- src/app.js
+++ src/app.js
@@ -31,9 +31,9 @@ app.use(cors())
 app.use(compress())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(favicon(path.join(app.get('public'), 'favicon.ico')))
+app.use(favicon(path.join('public/spa', 'favicon.ico')))
 // Host the public folder
-app.use('/', express.static(app.get('public')))
+app.use('/', express.static('public/spa'))
 
 // Set up Plugins and providers
 app.configure(socketio())
